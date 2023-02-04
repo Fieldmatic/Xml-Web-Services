20a671a35185f2f3b04bfff989c4da2c77285138
ivana.ste <stevanovic.sw69.2019@uns.ac.rs>
2023-02-04 19:54:17 +0100

    a1 service

--- frontend/src/app/a1/services/a1.service.ts
+++ frontend/src/app/a1/services/a1.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { AppConfig } from 'src/app/appConfig/appconfig.interface';
 import { APP_SERVICE_CONFIG } from 'src/app/appConfig/appconfig.service';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,21 @@ export class A1Service {
     private router: Router,
     @Inject(APP_SERVICE_CONFIG) private config: AppConfig
   ) {}
+
+  sacuvajA1Obrazac(request: string) {
+    return this.http.post(
+      this.config.autorskoPravoEndpoint + 'autorska-prava',
+      request,
+      {
+        observe: 'body',
+        responseType: 'text',
+        headers: {
+          'Content-Type': 'application/xml',
+          Accept: 'application/xml',
+        },
+      }
+    );
+  }
 
   ucitajFajl(file: File, type: boolean) {
     const exampleFormData = new FormData();
